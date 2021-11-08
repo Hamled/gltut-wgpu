7437b12041c9a1875c6c97882125cde37f67bf9c
Charles Ellis <hamled@hamled.dev>
2021-11-07 23:50:38 -0800

    Empty winit event loop
    We don't handle any events yet, and to avoid busy looping we set the
    control flow to wait until we have additional events.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use winit::{event_loop::EventLoop, window::WindowBuilder};
+use winit::{
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 fn main() {
     let event_loop = EventLoop::new();
     let _window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |_event, _target, control_flow| {
+        *control_flow = ControlFlow::Wait;
+    });
 }
