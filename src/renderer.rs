17891c1fc06715b01f432114abf5600d1fdc1ed5
Charles Ellis <hamled@hamled.dev>
2021-11-08 01:42:31 -0800

    Renderer has device and queue
    A bit more of the legwork necessary to be able to use the GPU.

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,18 +1,40 @@
-use wgpu::{Instance, Surface};
+use pollster::block_on;
+use wgpu::{Device, Queue, Surface};
 use winit::window::Window;
 
 pub struct Renderer<'window> {
     _surface: Surface,
+    _device: Device,
+    _queue: Queue,
     phantom_window: std::marker::PhantomData<&'window Window>,
 }
 
 impl<'window> Renderer<'window> {
     pub fn new(window: &'window Window) -> Self {
-        let instance = Instance::new(wgpu::Backends::VULKAN);
+        let instance = wgpu::Instance::new(wgpu::Backends::VULKAN);
         let _surface = unsafe { instance.create_surface(window) };
+
+        let adapter = block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&_surface),
+            force_fallback_adapter: false,
+        }))
+        .unwrap();
+
+        let (_device, _queue) = block_on(adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                label: Some("gltut Device"),
+                features: wgpu::Features::default(),
+                limits: wgpu::Limits::default(),
+            },
+            None,
+        ))
+        .unwrap();
 
         Renderer {
             _surface,
+            _device,
+            _queue,
             phantom_window: std::marker::PhantomData,
         }
     }
