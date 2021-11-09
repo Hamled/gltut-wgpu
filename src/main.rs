447a0e048103293ee0febf76645bd0a77d510567
Charles Ellis <hamled@hamled.dev>
2021-11-08 18:10:06 -0800

    Clear the output to a fixed color on every event
    This isn't strictly necessary because our application doesn't do
    anything yet. We could have cleared the output once before the event
    handler and gotten the same result, for now.

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,12 @@ fn main() {
     let mut app = App::new(window);
 
     event_loop.run(move |_event, _target, control_flow| {
+        app.renderer.clear_immediate(wgpu::Color {
+            r: 0.1,
+            g: 0.2,
+            b: 0.3,
+            a: 1.0,
+        });
         *control_flow = ControlFlow::Wait;
     });
 }
